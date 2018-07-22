dacd33ecbc6736dbc256348711a95e37fb9bf397
antarikshgupta71 <antarikshgupta71@gmail.com>
2018-07-22 13:08:54 +0530

    Main Activity fixed. Scroll bar added

--- app/src/main/java/com/example/aloofwillow/nationalanthems/MainActivity.java
+++ app/src/main/java/com/example/aloofwillow/nationalanthems/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ScrollBar;
 
 public class MainActivity extends AppCompatActivity {
     EditText username,password;
